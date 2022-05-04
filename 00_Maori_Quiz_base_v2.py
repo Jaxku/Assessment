cdece347f5eecddbf171511259cc1bab3ef51a2d
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-05-05 09:35:31 +1200

    changes

---
+++ 00_Maori_Quiz_base_v2.py
@@ -0,0 +1,47 @@
+"""yes_no_checker_v1
+Adding instructions and starting main routine"""
+
+
+# function to format text output
+def formatter(symbol, text):
+    sides = symbol * 3
+    formatted_text = f"{sides} {text} {sides}"
+    top_bottom = symbol * len(formatted_text)
+    return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
+
+
+# yes/no checking function for instructions
+def yes_no(question_text):
+    while True:
+        show_instructions = ""
+        while show_instructions != "x":
+            # Ask the user if they have played before
+            show_instructions = input('Have you taken this quiz before? (Yes/No): ').lower()
+
+            # If they say yes, output "Program Continues"
+            if show_instructions == 'yes' or show_instructions == "y":
+                print("-Program continues-")
+                break
+
+            # If they say no, output "Display Instructions"
+            elif show_instructions == 'no' or show_instructions == "n":
+                print("-Instructions for program-")
+
+            # Otherwise - show error
+            else:
+                print("Error: Please put either Yes or No")
+
+            print(f"You entered '{show_instructions}'")
+
+
+# Instructions for the quiz
+
+
+# main routine
+print(formatter("-", "Welcome to the Maori Number Quiz"))
+print()
+
+show_instructions = yes_no("Have you played this game before? (Yes/No): ")
+
+if show_instructions == "No":
+    instructions()
--- instructions_v1.py
+++ instructions_v1.py
@@ -1,2 +1,27 @@
 """instructions_V1
 Base instructions for game in final code this will be used a function so it can be called in with the main routine"""
+
+
+# this function not apart of this code but is necessary for testing this code and for it to operate this function will be placed appropriately in the final Quiz file
+def formatter(symbol, text):
+    sides = symbol * 3
+    formatted_text = f"{sides} {text} {sides}"
+    top_bottom = symbol * len(formatted_text)
+    return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
+
+
+def instructions():
+    print()
+    print(formatter("*", "Instructions"))
+    print()
+    print("You will be tested on Maori numbers 1-10 in 10 questions these will be randomized")
+    print()
+    print("Every question you get right will be 1 point. You need to get 7 points correct to pass.")
+    print()
+    print("This means you will need to get 7 Questions correct to pass the quiz")
+    print()
+    print("To answer question simply type in your answer and press enter (This quiz is not case sensitive)")
+
+
+# main routine (This is required to test the code since the Instructions code is a function)
+instructions()
