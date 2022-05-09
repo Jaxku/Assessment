c51e9c9753ee00dd41ecd6bda113524fe33bfd44
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-05-09 22:10:30 +1200

    Update 02_what_is_your_name_v3.py

--- 02_what_is_your_name_v3.py
+++ 02_what_is_your_name_v3.py
@@ -8,6 +8,6 @@ if len(user_name) > 35:
     print("Please enter a name under 36 characters.")
 if len(user_name) < 1:
     print("Please enter a name")
-if not re.compile("^[a-z],[-],[ ]*$", user_name): #the only type of characters allowed
+if not re.match("^[a-z],[-],[ ]*$", user_name): #the only type of characters allowed
     print("Only A-Z and hyphens are allowed!")
 
