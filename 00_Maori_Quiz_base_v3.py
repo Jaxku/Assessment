6dc5ac7bf3f7877bfce3493f5e7eaea4fb0a42cd
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-05-11 10:11:51 +1200

    fixed yes/no added if player is ready

--- 00_Maori_Quiz_base_v3.py
+++ 00_Maori_Quiz_base_v3.py
@@ -28,32 +28,42 @@ def name(user_name):
 # yes/no checking function for instructions
 def yes_no(question_text):
     while True:
-        show_instructions = ""
-        while show_instructions != "x":
-            # Ask the user if they have played before
-            show_instructions = input('Have you taken this quiz before? (Yes/No): ').lower()
 
-            # If they say yes, output "Program Continues"
-            if show_instructions == 'yes' or show_instructions == "y":
-                print("-Program continues-")
-                break
+        # Ask the user if they have played before
+        answer = input(question_text).lower()
 
-            # If they say no, output "Display Instructions"
-            elif show_instructions == 'no' or show_instructions == "n":
-                print("-Instructions for program-")
+        # If they say yes, output "Program Continues"
+        if answer == 'yes' or answer == "y":
+            answer = "Yes"
+            return answer
 
-            # Otherwise - show error
-            else:
-                print("Error: Please put either Yes or No")
+        # If they say no, output "Display Instructions"
+        elif answer == 'no' or answer == "n":
+            answer = "No"
+            return answer
 
-            print(f"You entered '{show_instructions}'")
+        # Otherwise - show error
+        else:
+            print("Error: Please put either Yes or No")
 
 
 # Instructions for the quiz
+def instructions():
+    print()
+    print(formatter("*", "Instructions"))
+    print()
+    print("You will be tested on Maori numbers 1-10 in 10 questions these will be randomized")
+    print()
+    print("Every question you get right will be 1 point. You need to get 7 points correct to pass.")
+    print()
+    print("This means you will need to get 7 Questions correct to pass the quiz")
+    print()
+    print("To answer question simply type in your answer and press enter (This quiz is not case sensitive)")
 
 
 # main routine
-print(formatter("-", "Welcome to the Maori Number Quiz\n"))
+print(formatter("-", "Welcome to the Maori Number Quiz"))
+print()
 user = input("What is your name? ")
 name(user)
 print(f"Welcome to the Maori Number Quiz {user}")
@@ -61,3 +71,9 @@ show_instructions = yes_no("Have you played this game before? (Yes/No): ")
 
 if show_instructions == "No":
     instructions()
+    print()
+while True:
+    player_ready = yes_no("Are you ready to play? (Yes/No): ")
+    if player_ready == "Yes":
+        print("player ready")
+        break
