f3dbf413581cb94e0cb75ce5b5bcf1a9b9cf92eb
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-05-16 12:26:40 +1200

    Pushing first version of the quiz script

--- 03_quiz_v1.py
+++ 03_quiz_v1.py
@@ -1,23 +1,29 @@
+# Using a template provided I have adjusted it to fit the Maori Number Quiz
 import random
-def get_name():
-    name_local=input("What is your name: ")
-    return name_local
-def get_age():
-    age_local=int(input("How old are you: "))
-    return age_local
-#Main routine
-name=get_name ()#1st function
-age=get_age()#2nd function
-print(f"\nHi {name}. At {age} years old, you might find thisabit easy. \n"
-      "\nAnyway, this isatest about days of the week. \n"
-      "You will need to enter the abbreviation which applies
-      "e.g. Fri for Friday \n")
-days=[["Monday", "Mon"], ["Tuesday", "Tue"], ["Wednesday", "Wed"]]
-random.shuffle(days)
-foriin days:
-    answer=input (f"Enter the abbreviation which applies to {i[0]}: ")
-                  i[1]:
-        print("##### CORRECT! ######\n")
-    if answer ==
-    else:
-        print("XXXXX INCORRECT! XXXXX\n")
+
+# This uses two ordinary (1-dimensional) lists
+
+print("Let's begin.\n"
+      "\n"
+      "You will be shown numbers in English and you need to convert them into Maori.\n"
+      "\n"
+      "You don't need to the put the line (macron) over the letter.\n"
+      " ")
+
+# 1st list
+numbers_english = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+# 2nd list
+number_maori = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau"]
+
+question = random.choice(numbers_english)
+attempt = input(f"What is the Maori number for {question}: ").lower()
+
+# Using the index position of the question in one list to find the corresponding
+# index position of the answer
+answer_index = numbers_english.index(question)
+answer = number_maori[answer_index]
+# Compare the attempt to see if it matches the correct answer
+if attempt == answer:
+    print("##### CORRECT! ######\n")
+else:
+    print("XXXXX INCORRECT! XXXXX\n")
