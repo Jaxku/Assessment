206b68763e33f255b68dfa7a4a3056e89ed189fa
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-05-16 12:38:07 +1200

    finished v2 and fixed v1
    03_quiz

--- 03_quiz_v2.py
+++ 03_quiz_v2.py
@@ -1,16 +1,16 @@
-"""03_quiz_v1
-Using a template provided I have adjusted to fit the Maori Number Quiz"""
+"""03_quiz_v2
+Based from 03_quiz_v1 adding scoring system"""
 
 import random
 
 # This uses two ordinary (1-dimensional) lists
-
+user_score = 0
 print("Let's begin.\n"
       "\n"
       "You will be shown numbers in English and you need to convert them into Maori.\n"
-      "\n"
+      "\n"  # used to space out each line by another line
       "You don't need to the put the line (macron) over the letter.\n"
-      " ")
+      " ")  # Briefing the user on what is about to happen in the test
 
 # 1st list
 numbers_english = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
@@ -27,5 +27,10 @@ answer = number_maori[answer_index]
 # Compare the attempt to see if it matches the correct answer
 if attempt == answer:
     print("##### CORRECT! ######\n")
+    user_score = user_score + 1
+    print(f"Your current score is {user_score}")
 else:
     print("XXXXX INCORRECT! XXXXX\n")
+    print("No points have been awarded, better luck next time.")
+    print(f"Your current score is {user_score}")
+
