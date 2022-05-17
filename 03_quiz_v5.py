ff88052e2f36275b248e5370c4d411567f83620d
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-05-18 10:30:02 +1200

    wefwef

--- 03_quiz_v5.py
+++ 03_quiz_v5.py
@@ -3,6 +3,14 @@ Adding end screen, formatting"""
 
 import random
 
+
+def formatter(symbol, text):  # Formatter script for text, needed for testing but not directly apart of the quiz function
+    sides = symbol * 3
+    formatted_text = f"{sides} {text} {sides}"
+    top_bottom = symbol * len(formatted_text)
+    return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
+
+
 # This uses two ordinary (1-dimensional) lists
 print("Let's begin.\n"
       "\n"
@@ -17,11 +25,12 @@ numbers_english = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight
 number_maori = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau"]
 
 play_again = " "
-rounds_played = 0
+rounds_played = 1
 user_score = 0
 
-while rounds_played < 11:  # Limits quiz to 10 rounds
+while rounds_played < 10:  # Limits quiz to 10 rounds
     question = random.choice(numbers_english)
+    print(formatter("✓", f"Question {rounds_played}"))
     attempt = input(f"What is the Maori number for {question}: ").lower()
 
     # Using the index position of the question in one list to find the corresponding
@@ -30,17 +39,17 @@ while rounds_played < 11:  # Limits quiz to 10 rounds
     answer = number_maori[answer_index]
     # Compare the attempt to see if it matches the correct answer
     if attempt == answer:
-        print("##### CORRECT! ######\n")
+        print(formatter("-", f"CORRECT"))
         print("You have earned 1 point. Good work!")
         user_score = user_score + 1  # gives the user a point for getting the question correct
-        rounds_played += 1  # keep track of rounds
         print(f"Your current score is {user_score}")
         print(f"You are on round {rounds_played}/10")
+        rounds_played += 1  # keep track of rounds
         print()  # adds some spacing between questions
     else:
         print("XXXXX INCORRECT! XXXXX\n")
         print("No points have been awarded, better luck next time.")
-        rounds_played += 1  # keep track of rounds
         print(f"Your current score is {user_score}")
         print(f"You are on round {rounds_played}/10")
+        rounds_played += 1  # keep track of rounds
         print()  # adds some spacing between questions
