b44863cb3fa59327708985c6841b8f2f0ff0ac49
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-05-17 15:15:13 +1200

    started v4

---
+++ 03_quiz_v4.py
@@ -0,0 +1,45 @@
+"""03_quiz_v2
+Adding rounds"""
+
+import random
+
+# This uses two ordinary (1-dimensional) lists
+print("Let's begin.\n"
+      "\n"
+      "You will be shown numbers in English and you need to convert them into Maori.\n"
+      "\n"  # used to space out each line by another line
+      "You don't need to the put the line (macron) over the letter.\n"
+      " ")  # Briefing the user on what is about to happen in the test
+
+# 1st list
+numbers_english = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+# 2nd list
+number_maori = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau"]
+
+play_again = " "
+rounds_played = 0
+user_score = 0
+
+while rounds_played < 10:
+    question = random.choice(numbers_english)
+    attempt = input(f"What is the Maori number for {question}: ").lower()
+
+    # Using the index position of the question in one list to find the corresponding
+    # index position of the answer
+    answer_index = numbers_english.index(question)
+    answer = number_maori[answer_index]
+    # Compare the attempt to see if it matches the correct answer
+    if attempt == answer:
+        print("##### CORRECT! ######\n")
+        print("You have earned 1 point. Good work!")
+        user_score = user_score + 1  # gives the user a point for getting the question correct
+        rounds_played += 1  # keep track of rounds
+        print(f"Your current score is {user_score}")
+        print(f"You are on")
+        print()  # adds some spacing between questions
+    else:
+        print("XXXXX INCORRECT! XXXXX\n")
+        print("No points have been awarded, better luck next time.")
+        rounds_played += 1  # keep track of rounds
+        print(f"Your current score is {user_score}")
+        print()  # adds some spacing between questions
