b761fcf482a511dda79e06d85904772270b9a63a
Jäcḳ <52820208+Jaxku@users.noreply.github.com>
2022-05-22 13:35:32 +1200

    Create 01_yes_no_v5.py

---
+++ 01_yes_no_v5.py
@@ -0,0 +1,26 @@
+"""yes_no_checker_v5
+This is the final version of the yes/no checker this version is a function and can be used anywhere in the code
+unlike the other versions which were developed with function in mind which is to ask if they have played the quiz before
+which was good for testing."""
+
+
+# yes/no checking function for instructions
+def yes_no(question_text):
+    while True:
+
+        # Ask the user if they have played before
+        answer = input(question_text).lower()
+
+        yes_responses = ["yes", "a", "ae", "y", "true"]  # supports english and te reo
+        no_responses = ["no", "kao", "n", "false"]  # supports english and te reo
+
+        # If they say yes, output "Program Continues"
+        if answer in yes_responses:
+            return True
+
+        # If they say no, output "Display Instructions"
+        if answer in no_responses:
+            return False
+
+        # Otherwise - show error
+        print("Error: Please put either Yes or No")
