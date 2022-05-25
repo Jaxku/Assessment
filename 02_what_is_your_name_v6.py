d6c7126785521c6e8a63145dad9e4e4fd4cafb60
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-05-26 09:31:26 +1200

    last and final update

--- 02_what_is_your_name_v6.py
+++ 02_what_is_your_name_v6.py
@@ -3,6 +3,7 @@
 based on v5, refined a loop to make sure that if a user gives an invalid input it can ask them again.
 This Version does not work in this file I can only be run in 00_Maori_Quiz_base_v5.py due to the use of functions"""
 
+import re
 
 # Allows users to input name
 # if it follows the set rules to stop abuse of this function
@@ -20,8 +21,9 @@ def name():
     else:
         return user_name
  
- # (from main loop)
- while True:
-    user = name()
-    if user:
-       break
+# (from main loop)
+    while True:
+        user = name()
+        if user:
+            break
+
